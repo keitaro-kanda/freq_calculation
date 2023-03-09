4fa4dbf06b069cb87c15519dc205018d88f5c5b3
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-03-09 15:26:09 +0900

    高度を計算に実装

--- recieved_power_dB.py
+++ recieved_power_dB.py
@@ -10,7 +10,7 @@ with open('params.json') as f:
     params = json.load(f)
 
 
-# 定数の設定
+# 変数の定義
 c = params['speed_of_light'] #真空中の光速[m/s]
 pi = np.pi  # 円周率π
 
@@ -42,18 +42,20 @@ def calc_Pr(gain, noise_level_W):
     print('reflect:', Gamma_r)
     print('through:',Gamma_t)
 
+
     #周波数、深さ
     freq = np.arange(freq_min, freq_max, freq_step)
     depth = np.arange(depth_min, depth_max, depth_step)
     # Rとfのメッシュグリッドを生成
     f_mesh, R_mesh = np.meshgrid(freq, depth)
 
+
     # ノイズレベルの算出[dB]
     noise_dB = 10*np.log10(noise_level / Pt)
 
 
     # 受信強度の計算[dB]
-    Pr = 10.0 * np.log10(gain**2 * c**2 / (4.0 * pi)**3 / R_mesh**4 / (f_mesh*10**6)**2 * sigma) + \
+    Pr = 10.0 * np.log10(gain**2 * c**2 / (4.0 * pi)**3 / (R_mesh + altitude)**4 / (f_mesh*10**6)**2 * sigma) + \
         10.0 * np.log10(Gamma_r * Gamma_t**4) - \
         (0.091 * f_mesh * np.sqrt(epsilon_r) * loss_tangent) * 2.0 * R_mesh
     Pr_detectability = Pr - noise_dB
@@ -61,7 +63,7 @@ def calc_Pr(gain, noise_level_W):
 
     # 特定の周波数で固定してdepth対dBのプロットを作る
     def calc_Pr_certain_freq(certain_f):
-        Pr_freq = 10.0 * np.log10(gain**2 * c**2 / (4.0 * pi)**3 / depth**4 / (certain_f*10**6)**2 * sigma) + \
+        Pr_freq = 10.0 * np.log10(gain**2 * c**2 / (4.0 * pi)**3 / (depth + altitude)**4 / (certain_f*10**6)**2 * sigma) + \
         10.0 * np.log10(Gamma_r * Gamma_t**4) - \
         (0.091 * certain_f * np.sqrt(epsilon_r) * loss_tangent) * 2.0 * depth
         Pr_freq_detectability = Pr_freq - noise_dB
