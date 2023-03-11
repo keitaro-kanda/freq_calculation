45237ad5ab94f8268b4de2dffccad404bced915c
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-03-11 20:22:41 +0900

    outputフォルダ名を変更

--- recieved_power_dB.py
+++ recieved_power_dB.py
@@ -83,7 +83,7 @@ def calc_Pr():
 
 
     # アウトプットを保存するフォルダを作成
-    folder_name = "output_recieved_power/gain"+str(gain)+"_altitude"+str(altitude)
+    folder_name = "output_recieved_power/gain"+str(gain)+"_altitude"+str(altitude)+'transmssion'+str(Pt)'noise'+str(noise_dB)
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
