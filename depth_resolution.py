1a251cc53e96fa7007972a624fdd17a4a613ffe8
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-03-16 18:52:30 +0900

    figフォルダの削除、"output_resolution"の作成

--- depth_resolution.py
+++ depth_resolution.py
@@ -5,29 +5,33 @@ import numpy as np
 
 #誘電率
 epsilon1 = 4.0
-epsilon0 = 1.0
+epsilon2 = 5.0
+epsilon3 = 6.0
+
 #光速
 c = 299792458 #[m/s]
 
 
-df = np.arange(1, 200) #MHz
+f0 = np.arange(1, 200) # 中心周波数
 
-dR_basalt = c/(2*np.sqrt(epsilon1)*df*10**6)
-dR_vacuum = c/(2*np.sqrt(epsilon0)*df*10**6)
+dR1 = c / (2*np.sqrt(epsilon1) * f0*10**6)
+dR2 = c / (2*np.sqrt(epsilon2) * f0*10**6)
+dR3 = c / (2*np.sqrt(epsilon3) * f0*10**6)
 
 
 #描画
 plt.figure(figsize=(8, 8))
-#plt.plot(df, dR_basalt, label='in basalt')
-plt.plot(df, dR_vacuum)
+plt.plot(f0, dR1, label='$\varepsilon_r = 4.0$')
+plt.plot(f0, dR2, label='$\varepsilon_r = 5.0$')
+plt.plot(f0, dR3, label='$\varepsilon_r = 6.0$')
 
 #グラフの体裁
 plt.title('Depth Resolution', fontsize=20)
-plt.xlabel('$\Delta f$ [MHz]', fontsize=15)
-plt.ylabel('$\Delta R$ [m]', fontsize=15)
+plt.xlabel('Center Frequency [MHz]', fontsize=15)
+plt.ylabel('Depth Resolution [m]', fontsize=15)
 plt.tick_params(labelsize=15)
 plt.ylim(0, 5)
 plt.grid()
-#plt.legend(fontsize='10')
-plt.savefig('fig/Depth_resolutin.png')
+plt.legend(fontsize='10')
+plt.savefig('output_resolution/Depth_resolutin.png')
 plt.show()
