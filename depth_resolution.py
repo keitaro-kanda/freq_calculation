5e06ba0efbeebe4b742c683804ee01cf3070d65f
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-03-16 18:59:04 +0900

    range reslutionの計算を中心周波数をもとにした計算に変更

--- depth_resolution.py
+++ depth_resolution.py
@@ -21,16 +21,15 @@ dR3 = c / (2*np.sqrt(epsilon3) * f0*10**6)
 
 #描画
 plt.figure(figsize=(8, 8))
-plt.plot(f0, dR1, label='$\varepsilon_r = 4.0$')
-plt.plot(f0, dR2, label='$\varepsilon_r = 5.0$')
-plt.plot(f0, dR3, label='$\varepsilon_r = 6.0$')
+plt.plot(f0, dR1, label=r'$\varepsilon _r = 4.0$')
+plt.plot(f0, dR2, label=r'$\varepsilon_r = 5.0$')
+plt.plot(f0, dR3, label=r'$\varepsilon_r = 6.0$')
 
 #グラフの体裁
 plt.title('Depth Resolution', fontsize=20)
 plt.xlabel('Center Frequency [MHz]', fontsize=15)
 plt.ylabel('Depth Resolution [m]', fontsize=15)
 plt.tick_params(labelsize=15)
-plt.ylim(0, 5)
 plt.grid()
 plt.legend(fontsize='10')
 plt.savefig('output_resolution/Depth_resolutin.png')
