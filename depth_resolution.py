045fdebe6f5f242ba5b63d23a599a2c487cbce39
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-03-29 17:36:26 +0900

    ε=1.0の追加

--- depth_resolution.py
+++ depth_resolution.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #誘電率
-epsilon1 = 4.0
-epsilon2 = 5.0
+epsilon1 = 1.0
+epsilon2 = 4.0
 epsilon3 = 6.0
 
 #光速
@@ -24,9 +24,10 @@ plt.figure(figsize=(25, 16))
 plt.subplots_adjust(wspace=0.5)
 plt.subplots_adjust(hspace=0.3)
 
+# ----1枚目----
 plt.subplot(2, 3, 1)
-plt.plot(f0, dR1, label=r'$\varepsilon _r = 4.0$')
-#plt.plot(f0, dR2, label=r'$\varepsilon_r = 5.0$')
+plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
+plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
 #plt.plot(f0, dR3, label=r'$\varepsilon_r = 6.0$')
 
 plt.title(' (a) Depth Resolution (Overview)', fontsize=24)
@@ -35,10 +36,13 @@ plt.ylabel('Depth Resolution [m]', fontsize=20)
 plt.tick_params(labelsize=20)
 plt.yscale('log')
 plt.grid()
+plt.legend(fontsize='20')
 
 
+# ----2枚目----
 plt.subplot(2, 3, 4)
-plt.plot(f0, dR1, label=r'$\varepsilon _r = 4.0$')
+plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
+plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
 
 plt.title('(b) Depth Resolution (Detail of 1-10 MHz)', fontsize=24)
 plt.xlabel('Center Frequency [MHz]', fontsize=20)
@@ -46,32 +50,39 @@ plt.ylabel('Depth Resolution [m]', fontsize=20)
 plt.tick_params(labelsize=20)
 plt.xlim(1, 10)
 plt.grid()
+plt.legend(fontsize='20')
 
+
+# ----3枚目----
 plt.subplot(2, 3, 5)
-plt.plot(f0, dR1, label=r'$\varepsilon _r = 4.0$')
+plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
+plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
 
 plt.title('(c) Depth Resolution (Detail of 10-75 MHz)', fontsize=24)
 plt.xlabel('Center Frequency [MHz]', fontsize=20)
 plt.ylabel('Depth Resolution [m]', fontsize=20)
 plt.tick_params(labelsize=20)
 plt.xlim(10, 75)
-plt.ylim(0, 20)
+#plt.ylim(0, 20)
 plt.grid()
+plt.legend(fontsize='20')
 
 
+# ----4枚目----
 plt.subplot(2, 3, 6)
-plt.plot(f0, dR1, label=r'$\varepsilon _r = 4.0$')
+plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
+plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
 
 plt.title('(d) Depth Resolution (Detail of 75-200 MHz)', fontsize=24)
 plt.xlabel('Center Frequency [MHz]', fontsize=20)
 plt.ylabel('Depth Resolution [m]', fontsize=20)
 plt.tick_params(labelsize=20)
 plt.xlim(75, 200)
-plt.ylim(0, 2.5)
+#plt.ylim(0, 2.5)
 plt.grid()
+plt.legend(fontsize='20')
 
-#グラフの体裁
-#plt.legend(fontsize='10')
+
 plt.savefig('output_resolution/Depth_resolutin.png')
 
 
