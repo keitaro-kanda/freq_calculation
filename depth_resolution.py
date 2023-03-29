5bd889a5ec36fee183953d14b7ed1d15e1425509
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-03-29 17:18:12 +0900

    delta fを0.5f0に変更

--- depth_resolution.py
+++ depth_resolution.py
@@ -14,9 +14,9 @@ c = 299792458 # [m/s]
 
 f0 = np.arange(1, 200, 0.1) # 中心周波数
 
-dR1 = c / (2*np.sqrt(epsilon1) * f0*10**6)
-dR2 = c / (2*np.sqrt(epsilon2) * f0*10**6)
-dR3 = c / (2*np.sqrt(epsilon3) * f0*10**6)
+dR1 = c / (2*np.sqrt(epsilon1) * f0 *10**6 * 0.5)
+dR2 = c / (2*np.sqrt(epsilon2) * f0 *10**6 * 0.5)
+dR3 = c / (2*np.sqrt(epsilon3) * f0 *10**6 * 0.5)
 
 
 #描画
@@ -26,10 +26,10 @@ plt.plot(f0, dR1, label=r'$\varepsilon _r = 4.0$')
 #plt.plot(f0, dR2, label=r'$\varepsilon_r = 5.0$')
 #plt.plot(f0, dR3, label=r'$\varepsilon_r = 6.0$')
 
-plt.title('Depth Resolution (Overview)', fontsize=20)
-plt.xlabel('Center Frequency [MHz]', fontsize=15)
-plt.ylabel('Depth Resolution [m]', fontsize=15)
-plt.tick_params(labelsize=15)
+plt.title(' (a) Depth Resolution (Overview)', fontsize=24)
+plt.xlabel('Center Frequency [MHz]', fontsize=20)
+plt.ylabel('Depth Resolution [m]', fontsize=20)
+plt.tick_params(labelsize=20)
 plt.yscale('log')
 plt.grid()
 
@@ -37,34 +37,34 @@ plt.grid()
 plt.subplot(2, 3, 4)
 plt.plot(f0, dR1, label=r'$\varepsilon _r = 4.0$')
 
-plt.title('Depth Resolution (Detail of 1-10 MHz)', fontsize=20)
-plt.xlabel('Center Frequency [MHz]', fontsize=15)
-plt.ylabel('Depth Resolution [m]', fontsize=15)
-plt.tick_params(labelsize=15)
+plt.title('(b) Depth Resolution (Detail of 1-10 MHz)', fontsize=24)
+plt.xlabel('Center Frequency [MHz]', fontsize=20)
+plt.ylabel('Depth Resolution [m]', fontsize=20)
+plt.tick_params(labelsize=20)
 plt.xlim(1, 10)
 plt.grid()
 
 plt.subplot(2, 3, 5)
 plt.plot(f0, dR1, label=r'$\varepsilon _r = 4.0$')
 
-plt.title('Depth Resolution (Detail of 10-75 MHz)', fontsize=20)
-plt.xlabel('Center Frequency [MHz]', fontsize=15)
-plt.ylabel('Depth Resolution [m]', fontsize=15)
-plt.tick_params(labelsize=15)
+plt.title('(c) Depth Resolution (Detail of 10-75 MHz)', fontsize=24)
+plt.xlabel('Center Frequency [MHz]', fontsize=20)
+plt.ylabel('Depth Resolution [m]', fontsize=20)
+plt.tick_params(labelsize=20)
 plt.xlim(10, 75)
-plt.ylim(0, 10)
+plt.ylim(0, 20)
 plt.grid()
 
 
 plt.subplot(2, 3, 6)
 plt.plot(f0, dR1, label=r'$\varepsilon _r = 4.0$')
 
-plt.title('Depth Resolution (Detail of 75-200 MHz)', fontsize=20)
-plt.xlabel('Center Frequency [MHz]', fontsize=15)
-plt.ylabel('Depth Resolution [m]', fontsize=15)
-plt.tick_params(labelsize=15)
+plt.title('(d) Depth Resolution (Detail of 75-200 MHz)', fontsize=24)
+plt.xlabel('Center Frequency [MHz]', fontsize=20)
+plt.ylabel('Depth Resolution [m]', fontsize=20)
+plt.tick_params(labelsize=20)
 plt.xlim(75, 200)
-plt.ylim(0, 1.2)
+plt.ylim(0, 2.5)
 plt.grid()
 
 #グラフの体裁
