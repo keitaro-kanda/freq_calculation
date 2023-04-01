e13738c805f701ceca9bf47251ecf01533810ebc
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-04-01 21:06:23 +0900

    savefig追加

--- calc_dPr_dd.py
+++ calc_dPr_dd.py
@@ -28,33 +28,48 @@ d = np.arange(0.1, 100, 0.1)
 f_list = [5, 25, 50, 75, 100, 150]
 
 # 高度設定
-altitude = 25 # [km]
-h = altitude * 1000 # [m]
+#altitude = 25 # [km]
+#h = altitude * 1000 # [m]
 
 
-plt.figure(figsize=(10, 4))
+plt.figure(figsize=(14, 14))
+plt.subplots_adjust(wspace=0.7)
+plt.subplots_adjust(hspace=0.3)
+
     # dPr/ddの式を計算し、グラフをプロット
-for f in f_list:
-    ddPr = 20 * ((-2/(d+h)) + (1/d)) / np.log(10) - f * 3.64e-3
-    plt.plot(d, ddPr, label=f"f={f}")
+for i in [1, 2, 3]:
+    for f in f_list:
+        if i==1:
+            altitude=25
+        elif i==2:
+            altitude=50
+        elif i ==3:
+            altitude=100
+        h = altitude * 1000
+
+        ddPr = 20 * ((-2/(d+h)) + (1/d)) / np.log(10) - f * 3.64e-3
+        plt.subplot(3, 1, i)
+        plt.plot(d, ddPr, label=f"f={f}")
         
-    # dPrが0になるx座標を取得して、グラフ上に表示
-    hanbetsu = np.argmin(np.abs(ddPr))
-    x_intercept = d[hanbetsu]
-    #if  hanbetsu<0.1:
-    plt.text(x_intercept, 0, f"{x_intercept:.2f}", ha="center", va="center")
+        # dPrが0になるx座標を取得して、グラフ上に表示
+        hanbetsu = np.argmin(np.abs(ddPr))
+        x_intercept = d[hanbetsu]
+        #if  hanbetsu<0.1:
+        plt.text(x_intercept, 0, f"{x_intercept:.2f}", ha="center", va="center")
 
+        # グラフのタイトル、ラベル、凡例を設定
+        plt.title(r"h="+str(altitude)+"km", size=24)
+        plt.xlabel(r"$d$",size=20)
+        plt.ylabel(r"$\frac{d P_r}{d d}$", size=20)
+        plt.ylim(-0.1, 0.1)
+        plt.tick_params(labelsize=20)
+        plt.legend(fontsize=16)
 
-# グラフのタイトル、ラベル、凡例を設定
-plt.title(r"h="+str(altitude)+"km")
-plt.xlabel("d")
-plt.ylabel(r"$\frac{d P_r}{d d}$")
-plt.ylim(-0.1, 0.1)
-plt.legend()
+        # グリッドを表示
+        plt.grid()
 
-# グリッドを表示
-plt.grid()
-
+# グラフを保存
+plt.savefig('output_recieved_power/dPr_dd.png')
 # グラフを表示
 plt.show()
 
