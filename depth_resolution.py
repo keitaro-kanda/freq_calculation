afcef23c695e9de85e2321293a6149b4c2a861b8
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-04-05 17:48:49 +0900

    resolution_detectability.pyの諦め

--- depth_resolution.py
+++ depth_resolution.py
@@ -1,12 +1,5 @@
-from cProfile import label
-
 import matplotlib.pyplot as plt
 import numpy as np
-
-#誘電率
-epsilon1 = 1.0
-epsilon2 = 4.0
-epsilon3 = 6.0
 
 #光速
 c = 299792458 # [m/s]
@@ -14,76 +7,165 @@ c = 299792458 # [m/s]
 
 f0 = np.arange(1, 300, 0.1) # 中心周波数
 
-dR1 = c / (2*np.sqrt(epsilon1) * f0 *10**6 * 0.5)
-dR2 = c / (2*np.sqrt(epsilon2) * f0 *10**6 * 0.5)
-dR3 = c / (2*np.sqrt(epsilon3) * f0 *10**6 * 0.5)
+def calc_resolution():
+    #誘電率
+    epsilon1 = 1.0
+    epsilon2 = 4.0
 
 
-#描画
-plt.figure(figsize=(25, 16))
-plt.subplots_adjust(wspace=0.7)
-plt.subplots_adjust(hspace=0.3)
-
-# ----1枚目----
-plt.subplot(2, 3, 1)
-plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
-plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
-#plt.plot(f0, dR3, label=r'$\varepsilon_r = 6.0$')
-
-plt.title(' (a) Depth Resolution (Overview)', fontsize=24)
-plt.xlabel('Center Frequency [MHz]', fontsize=20)
-plt.ylabel('Depth Resolution [m]', fontsize=20)
-plt.tick_params(labelsize=20)
-plt.yscale('log')
-plt.grid()
-plt.legend(fontsize='20')
+    dR1 = c / (2*np.sqrt(epsilon1) * f0 *10**6 * 0.5)
+    dR2 = c / (2*np.sqrt(epsilon2) * f0 *10**6 * 0.5)
 
 
-# ----2枚目----
-plt.subplot(2, 3, 4)
-plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
-plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
+    #描画
+    plt.figure(figsize=(25, 16))
+    plt.subplots_adjust(wspace=0.7)
+    plt.subplots_adjust(hspace=0.3)
 
-plt.title('(b) Depth Resolution (Detail of 1-10 MHz)', fontsize=24)
-plt.xlabel('Center Frequency [MHz]', fontsize=20)
-plt.ylabel('Depth Resolution [m]', fontsize=20)
-plt.tick_params(labelsize=20)
-plt.xlim(1, 10)
-plt.grid()
-plt.legend(fontsize='20')
+    # ----1枚目----
+    plt.subplot(2, 3, 1)
+    plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
+    plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
 
-
-# ----3枚目----
-plt.subplot(2, 3, 5)
-plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
-plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
-
-plt.title('(c) Depth Resolution (Detail of 10-60 MHz)', fontsize=24)
-plt.xlabel('Center Frequency [MHz]', fontsize=20)
-plt.ylabel('Depth Resolution [m]', fontsize=20)
-plt.tick_params(labelsize=20)
-plt.xlim(10, 60)
-plt.ylim(0, 32)
-plt.grid()
-plt.legend(fontsize='20')
+    plt.title(' (a) Depth Resolution (Overview)', fontsize=24)
+    plt.xlabel('Center Frequency [MHz]', fontsize=20)
+    plt.ylabel('Depth Resolution [m]', fontsize=20)
+    plt.tick_params(labelsize=20)
+    plt.yscale('log')
+    plt.grid()
+    plt.legend(fontsize='20')
 
 
-# ----4枚目----
-plt.subplot(2, 3, 6)
-plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
-plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
+    # ----2枚目----
+    plt.subplot(2, 3, 4)
+    plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
+    plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
 
-plt.title('(d) Depth Resolution (Detail of 60-300 MHz)', fontsize=24)
-plt.xlabel('Center Frequency [MHz]', fontsize=20)
-plt.ylabel('Depth Resolution [m]', fontsize=20)
-plt.tick_params(labelsize=20)
-plt.xlim(60, 300)
-plt.ylim(0, 5.2)
-plt.grid()
-plt.legend(fontsize='20')
-
-
-plt.savefig('output_resolution/Depth_resolutin.png')
+    plt.title('(b) Depth Resolution (Detail of 1-10 MHz)', fontsize=24)
+    plt.xlabel('Center Frequency [MHz]', fontsize=20)
+    plt.ylabel('Depth Resolution [m]', fontsize=20)
+    plt.tick_params(labelsize=20)
+    plt.xlim(1, 10)
+    plt.grid()
+    plt.legend(fontsize='20')
 
 
-plt.show()
+    # ----3枚目----
+    plt.subplot(2, 3, 5)
+    plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
+    plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
+
+    plt.title('(c) Depth Resolution (Detail of 10-60 MHz)', fontsize=24)
+    plt.xlabel('Center Frequency [MHz]', fontsize=20)
+    plt.ylabel('Depth Resolution [m]', fontsize=20)
+    plt.tick_params(labelsize=20)
+    plt.xlim(10, 60)
+    plt.ylim(0, 32)
+    plt.grid()
+    plt.legend(fontsize='20')
+
+
+    # ----4枚目----
+    plt.subplot(2, 3, 6)
+    plt.plot(f0, dR1, label=r'$\varepsilon _r = 1.0$')
+    plt.plot(f0, dR2, label=r'$\varepsilon_r = 4.0$')
+
+    plt.title('(d) Depth Resolution (Detail of 60-300 MHz)', fontsize=24)
+    plt.xlabel('Center Frequency [MHz]', fontsize=20)
+    plt.ylabel('Depth Resolution [m]', fontsize=20)
+    plt.tick_params(labelsize=20)
+    plt.xlim(50, 300)
+    plt.ylim(0, 5.2)
+    plt.grid()
+    plt.legend(fontsize='20')
+
+
+    plt.savefig('output_resolution/Depth_resolutin.png')
+
+
+    plt.show()
+
+#calc_resolution()
+
+
+def calc_resolution_detectability():
+    # 誘電率
+    epsilon1 = 1.0
+
+    # 分解能 [m]
+    dR1 = c / (2*np.sqrt(epsilon1) * f0 *10**6 * 0.5)
+
+    # 検出可能なチューブの最小高さ
+    tube_h = dR1 * 3
+
+
+    #描画
+    plt.figure(figsize=(25, 16))
+    plt.subplots_adjust(wspace=0.7)
+    plt.subplots_adjust(hspace=0.3)
+
+    # ----1枚目----
+    plt.subplot(2, 3, 1)
+    plt.plot(f0, tube_h, label=r'$\varepsilon _r = 1.0$')
+    #plt.hlines(50, 0, 300, colors='r')
+
+    plt.title(' (a) Overview', fontsize=24)
+    plt.xlabel('Center Frequency [MHz]', fontsize=20)
+    plt.ylabel('Minimum Detactable Tube Height [m]', fontsize=20)
+    plt.tick_params(labelsize=20)
+    plt.yscale('log')
+    plt.grid()
+    #plt.legend(fontsize='20')
+
+
+    # ----2枚目----
+    plt.subplot(2, 3, 4)
+    plt.plot(f0, tube_h, label=r'$\varepsilon _r = 1.0$')
+    #plt.hlines(50, 0, 300, colors='r')
+
+    plt.title('(b) Detail of 1-10 MHz', fontsize=24)
+    plt.xlabel('Center Frequency [MHz]', fontsize=20)
+    plt.ylabel('Minimum Detactable Tube Height [m]', fontsize=20)
+    plt.tick_params(labelsize=20)
+    plt.xlim(1, 10)
+    plt.grid()
+    #plt.legend(fontsize='20')
+
+
+    # ----3枚目----
+    plt.subplot(2, 3, 5)
+    plt.plot(f0, tube_h, label=r'$\varepsilon _r = 1.0$')
+    #plt.hlines(50, 0, 300, colors='r')
+
+    plt.title('(c) Detail of 10-60 MHz', fontsize=24)
+    plt.xlabel('Center Frequency [MHz]', fontsize=20)
+    plt.ylabel('Minimum Detactable Tube Height [m]', fontsize=20)
+    plt.tick_params(labelsize=20)
+    plt.xlim(10, 60)
+    plt.ylim(0, 100)
+    plt.grid()
+    #plt.legend(fontsize='20')
+
+
+    # ----4枚目----
+    plt.subplot(2, 3, 6)
+    plt.plot(f0, tube_h, label=r'$\varepsilon _r = 1.0$')
+
+    plt.title('(d) Detail of 60-300 MHz', fontsize=24)
+    plt.xlabel('Center Frequency [MHz]', fontsize=20)
+    plt.ylabel('Minimum Detactable Tube Height [m]', fontsize=20)
+    plt.tick_params(labelsize=20)
+    plt.xlim(60, 300)
+    plt.ylim(0, 20)
+    plt.grid()
+    
+    #plt.legend(fontsize='20')
+
+
+    plt.savefig('output_resolution/resolution_detactability.png')
+
+
+    plt.show()
+
+
+calc_resolution_detectability()
--- resolution_detectability.py
+++ resolution_detectability.py
@@ -5,10 +5,10 @@ import numpy as np
 fc_range = np.arange(1, 301, 1) 
 
 # 洞窟の高さの範囲を設定 [m]
-h_range = np.arange(1, 101, 1)  
+h_range = np.arange(1, 101, 0.5)  
 
 # 検出可能かどうかの結果を格納する2次元配列を初期化
-fh_mesh = np.zeros((len(fc_range), len(h_range)))
+fh_mesh = np.zeros((len(h_range), len(fc_range)))
 
 # delta_fの定義
 #delta_f_range = 0.5 * fc_range
@@ -20,8 +20,8 @@ epsilon_r = 1.0
 c = 299792458
 
 # 中心周波数と洞窟の高さの組み合わせごとに検出可能かどうかを計算
-for i, fc in enumerate(fc_range):
-    for j, h in enumerate(h_range):
+for i, fc in enumerate(h_range):
+    for j, h in enumerate(fc_range):
         delta_f = 0.5 * fc * 10**6
         delta_R = c / (2 * delta_f * np.sqrt(epsilon_r))
         #print(f"fc={fc}, h={h}, delta_R={delta_R}")
@@ -30,13 +30,47 @@ for i, fc in enumerate(fc_range):
 
 #print(fh_mesh)
 # プロットを作成
+plt.figure(figsize=(20, 15))
+plt.subplots_adjust(wspace=0.7)
+plt.subplots_adjust(hspace=0.3)
+
+plt.subplot(2, 3, 1)
 plt.imshow(fh_mesh, origin='lower', 
            extent=[fc_range[0], fc_range[-1], h_range[0], h_range[-1]], 
            aspect='auto', cmap='coolwarm', vmin=0, vmax=1.0)
-plt.colorbar(label='Detectable')  # カラーバーを追加
+#plt.colorbar(label='Detectable')  # カラーバーを追加
 plt.xlabel('Center Frequency [MHz]')  # 横軸のラベルを設定
 plt.ylabel('Cave Height [m]')  # 縦軸のラベルを設定
 plt.title('Detectability of Caves by Ground Penetrating Radar')  # タイトルを設定
 plt.grid()
+
+plt.subplot(2, 3, 4)
+plt.imshow(fh_mesh[1:100, 1:50], origin='lower', 
+           #extent=[fc_range[0], fc_range[-1], h_range[0], h_range[-1]], 
+           aspect='auto', cmap='coolwarm', vmin=0, vmax=1.0)
+#plt.colorbar(label='Detectable')  # カラーバーを追加
+plt.xlabel('Center Frequency [MHz]')  # 横軸のラベルを設定
+plt.ylabel('Cave Height [m]')  # 縦軸のラベルを設定
+plt.title('Detectability of Caves by Ground Penetrating Radar')  # タイトルを設定
+plt.grid()
+
+plt.subplot(2, 3, 5)
+plt.imshow(fh_mesh[1:30, 50:150], origin='lower', 
+           #extent=[fc_range[0], fc_range[-1], h_range[0], h_range[-1]], 
+           aspect='auto', cmap='coolwarm', vmin=0, vmax=1.0)
+#plt.colorbar(label='Detectable')  # カラーバーを追加
+plt.xlabel('Center Frequency [MHz]')  # 横軸のラベルを設定
+plt.ylabel('Cave Height [m]')  # 縦軸のラベルを設定
+plt.title('Detectability of Caves by Ground Penetrating Radar')  # タイトルを設定
+plt.grid()
+
+plt.subplot(2, 3, 6)
+plt.imshow(fh_mesh[1:10, 150:300], origin='lower', 
+           #extent=[fc_range[0], fc_range[-1], h_range[0], h_range[-1]], 
+           aspect='auto', cmap='coolwarm', vmin=0, vmax=1.0)
+#plt.colorbar(label='Detectable')  # カラーバーを追加
+plt.xlabel('Center Frequency [MHz]')  # 横軸のラベルを設定
+plt.ylabel('Cave Height [m]')  # 縦軸のラベルを設定
+plt.title('Detectability of Caves by Ground Penetrating Radar')  # タイトルを設定
 plt.show()
 
