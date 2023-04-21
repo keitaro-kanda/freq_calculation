817acc5c87c427faee67220db2ff21bf964e014a
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-04-21 11:47:42 +0900

    rover_paramsnの名前変更

--- calc_detectability.py
+++ calc_detectability.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 選択するパラメータファイルの指定
-params_file = "rover2"  # orbiter/rover2
+params_file = "rover"  # orbiter/rover2
 
 # パラメータファイルの読み込み
 with open('params/'+params_file + '_params.json') as f:
