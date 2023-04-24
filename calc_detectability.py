3dcacfce94074747ea582c7e08409ddec0b6ec4f
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-04-24 16:58:01 +0900

    setting='LRS'の作成

--- calc_detectability.py
+++ calc_detectability.py
@@ -6,6 +6,7 @@ import numpy as np
 
 # 選択するパラメータファイルの指定
 params_file = "orbiter"  # orbiter/rover
+setting = 'LRS' # LRS/other
 
 # パラメータファイルの読み込み
 with open('params/'+params_file + '_params.json') as f:
@@ -13,7 +14,7 @@ with open('params/'+params_file + '_params.json') as f:
 
 
 # Cese Number
-case_num = 4
+case_num = 1
 # 中心周波数
 frequency = params['center_freq'+str(case_num)]
 
@@ -75,12 +76,15 @@ def calc_detectability():
                 (0.091 * frequency * np.sqrt(epsilon_r) * loss_tangent) * 2.0 * r
             Pr_detectability = Pr - noise_dB
             
-            freq_width = 0.5 * frequency
+            if setting == 'other':
+                freq_width = 0.5 * frequency
+            elif setting == 'LRS':
+                freq_width = 0.5 * frequency
             dR = c / (2*np.sqrt(epsilon_0) * freq_width * 10**6)
                 
 
             # 検出可能性の判定
-            if Pr_detectability > 0 and dR<=h/2:
+            if Pr_detectability > 0 and dR<=h/3:
                 fw_array[index_r, index_w] = 1
             else:
                 fw_array[index_r, index_w] = -1
@@ -91,8 +95,10 @@ calc_detectability()
 
 
 # アウトプットを保存するフォルダを作成
-folder_name = "output_detectability/"+str(params_file)+'/'+str(case_num)
-
+if setting == 'other':
+    folder_name = "output_detectability/"+str(params_file)+'/'+str(case_num)
+elif setting == 'LRS':
+    folder_name = "output_detectability/LRS"
 
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
@@ -112,11 +118,17 @@ plt.imshow(fw_array, origin='lower', cmap='coolwarm', aspect='equal',  vmin=-1, 
 #plt.colorbar()
 
 # タイトルの設定
-plt.title('Cese'+str(case_num), size=24)
+if setting == 'other':
+    plt.title('Cese'+str(case_num), size=24)
+    plt.xticks(np.arange(0, len(width), 2), width[::2])
+    plt.yticks(np.arange(0, len(roof), 2), roof[::2])
+elif setting == 'LRS':
+    plt.title('LRS', size=24)
+    plt.xticks(np.arange(1, len(width), 4), width[::4])
+    plt.yticks(np.arange(1, len(roof), 5), roof[::5])
+
 plt.xlabel('Tube Width [m]', size=20)  # 横軸のラベルを設定
 plt.ylabel('Roof Hight [m]', size=20)  # 縦軸のラベルを設定
-plt.xticks(np.arange(0, len(width), 2), width[::2])
-plt.yticks(np.arange(0, len(roof), 2), width[::2])
 plt.tick_params(axis='both', labelsize=15)
 plt.grid()
 
